4cae1db86aa28115de9e623a53dcca33210547d5
isajal07 <isajal07@iSajal07-2.local>
2020-06-06 16:29:16 +0545

    Prepared to deploy.

---
+++ client/build/precache-manifest.ceebb8258e43b7115d2ccbc057b5a23c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983a0a7e605944a063b6920561f7ade7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "546c5d11edb6ae6179c9",
+    "url": "/static/css/main.c65ec395.chunk.css"
+  },
+  {
+    "revision": "1e40b66b7c002d1eb770",
+    "url": "/static/js/2.e1f72eab.chunk.js"
+  },
+  {
+    "revision": "546c5d11edb6ae6179c9",
+    "url": "/static/js/main.823b5bad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
